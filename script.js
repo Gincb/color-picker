9d97684313b60175d79ec42e5b2f9f913f9cff42
Gintare <gintaar3@gmail.com>
2020-09-07 12:38:40 +0200

    get color and hex

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let colorPicker = document.querySelector("#colorpicker");
+let resultColor = document.querySelector("#resultColor");
+let resultHEX = document.querySelector(".hex");
+
+colorPicker.addEventListener("change", getColor);
+
+function getColor() {
+  resultColor.style.backgroundColor = colorPicker.value;
+  getHex();
+}
+
+function getHex() {
+  resultHEX.textContent = colorPicker.value;
+}
